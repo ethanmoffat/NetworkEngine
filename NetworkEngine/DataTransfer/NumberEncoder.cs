cf4077020a58535e9a77b3d5314338893c3bb315
Ethan Moffat <ethanmoffat@hotmail.com>
2019-03-21 17:48:29 -0700

    Add existing items from EOLib.Net

---
+++ NetworkEngine/DataTransfer/NumberEncoder.cs
@@ -0,0 +1,68 @@
+﻿// Original Work Copyright (c) Ethan Moffat 2014-2016
+// This file is subject to the GPL v2 License
+// For additional details, see the LICENSE file
+
+using System.Linq;
+
+namespace NetworkEngine.DataTransfer
+{
+    public class NumberEncoderService : INumberEncoder
+    {
+        private const int ONE_BYTE_MAX = 253;
+        private const int TWO_BYTE_MAX = 64009;
+        private const int THREE_BYTE_MAX = 16194277;
+
+        public byte[] EncodeNumber(int number, int size)
+        {
+            var unsigned = (uint) number;
+            var numArray = Enumerable.Repeat((uint)254, 4).ToArray();
+            var original = unsigned;
+
+            if (original >= THREE_BYTE_MAX)
+            {
+                numArray[3] = unsigned / THREE_BYTE_MAX + 1;
+                unsigned = unsigned % THREE_BYTE_MAX;
+            }
+
+            if (original >= TWO_BYTE_MAX)
+            {
+                numArray[2] = unsigned / TWO_BYTE_MAX + 1;
+                unsigned = unsigned % TWO_BYTE_MAX;
+            }
+
+            if (original >= ONE_BYTE_MAX)
+            {
+                numArray[1] = unsigned / ONE_BYTE_MAX + 1;
+                unsigned = unsigned % ONE_BYTE_MAX;
+            }
+
+            numArray[0] = unsigned + 1;
+
+            return numArray.Select(x => (byte)x)
+                           .Take(size)
+                           .ToArray();
+        }
+
+        public int DecodeNumber(params byte[] b)
+        {
+            for (int index = 0; index < b.Length; ++index)
+            {
+                if (b[index] == 254)
+                    b[index] = 1;
+                else if (b[index] == 0)
+                    b[index] = 128;
+                --b[index];
+            }
+
+            var retNum = 0;
+            if (b.Length > 3)
+                retNum += b[3]*THREE_BYTE_MAX;
+            if (b.Length > 2)
+                retNum += b[2]*TWO_BYTE_MAX;
+            if (b.Length > 1)
+                retNum += b[1]*ONE_BYTE_MAX;
+
+            return retNum + b[0];
+        }
+    }
+}
---
+++ NetworkEngine/DataTransfer/PacketBuilder.cs
@@ -0,0 +1,111 @@
+﻿// Original Work Copyright (c) Ethan Moffat 2014-2016
+// This file is subject to the GPL v2 License
+// For additional details, see the LICENSE file
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetworkEngine.DataTransfer
+{
+    public class PacketBuilder : IPacketBuilder
+    {
+        private const byte BREAK_STR_MAXVAL = 121;
+
+        private readonly IReadOnlyList<byte> _data;
+        private readonly INumberEncoder _encoderService;
+
+        public int Length => _data.Count;
+
+        public PacketFamily Family => (PacketFamily)_data[1];
+
+        public PacketAction Action => (PacketAction) _data[0];
+
+        public PacketBuilder()
+        {
+            _data = new List<byte>();
+            _encoderService = new NumberEncoderService();
+        }
+
+        public PacketBuilder(PacketFamily family, PacketAction action)
+        {
+            _data = new List<byte> { (byte) action, (byte) family };
+            _encoderService = new NumberEncoderService();
+        }
+
+        private PacketBuilder(IReadOnlyList<byte> data)
+        {
+            _data = data;
+            _encoderService = new NumberEncoderService();
+        }
+
+        public IPacketBuilder WithFamily(PacketFamily family)
+        {
+            var newData = new List<byte>(_data);
+            newData[1] = (byte)family;
+            return new PacketBuilder(newData);
+        }
+
+        public IPacketBuilder WithAction(PacketAction action)
+        {
+            var newData = new List<byte>(_data);
+            newData[0] = (byte)action;
+            return new PacketBuilder(newData);
+        }
+
+        public IPacketBuilder AddBreak()
+        {
+            return AddByte(byte.MaxValue);
+        }
+
+        public IPacketBuilder AddByte(byte b)
+        {
+            return AddBytes(new[] { b });
+        }
+
+        public IPacketBuilder AddChar(byte b)
+        {
+            return AddBytes(_encoderService.EncodeNumber(b, 1));
+        }
+
+        public IPacketBuilder AddShort(short s)
+        {
+            return AddBytes(_encoderService.EncodeNumber(s, 2));
+        }
+
+        public IPacketBuilder AddThree(int t)
+        {
+            return AddBytes(_encoderService.EncodeNumber(t, 3));
+        }
+
+        public IPacketBuilder AddInt(int i)
+        {
+            return AddBytes(_encoderService.EncodeNumber(i, 4));
+        }
+
+        public IPacketBuilder AddString(string s)
+        {
+            return AddBytes(Encoding.ASCII.GetBytes(s));
+        }
+
+        public IPacketBuilder AddBreakString(string s)
+        {
+            var sBytes = Encoding.ASCII.GetBytes(s);
+            var sList = sBytes.Select(b => b == byte.MaxValue ? BREAK_STR_MAXVAL : b).ToList();
+            sList.Add(byte.MaxValue);
+            return AddBytes(sList);
+        }
+
+        public IPacketBuilder AddBytes(IEnumerable<byte> bytes)
+        {
+            var list = new List<byte>(_data);
+            list.AddRange(bytes);
+            return new PacketBuilder(list);
+        }
+
+        public IPacket Build()
+        {
+            return new Packet(_data.ToList());
+        }
+    }
+}
